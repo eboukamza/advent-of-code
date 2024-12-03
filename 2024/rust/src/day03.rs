cfd02adf178e4a85a35f2eae3c9f3c24035bb221
Elias Zain <ezain2t@gmail.com>
2024-12-03 15:17:47 +0100

    day03

---
+++ 2024/rust/src/day03.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use regex::Regex;
+
+pub fn calculate_mul() -> i32 {
+    let contents = fs::read_to_string("./input3.txt").unwrap();
+    parse_and_execute(&contents)
+}
+
+pub fn calculate_mul_restricted() -> i32 {
+    let content = fs::read_to_string("./input3.txt").unwrap();
+    let restricted_content = remove_dont_ops(&content);
+    parse_and_execute(&restricted_content)
+}
+
+fn remove_dont_ops(code: &str) -> String {
+    let one_line = code.replace("\n", "");
+    let dont_regex = Regex::new(r"don't\(\).*?(?:do\(\)|$)").unwrap();
+    dont_regex.replace_all(&one_line, "").to_string()
+}
+
+fn parse_and_execute(code: &str) -> i32 {
+    let ops = scan_for_mul(code);
+    sum_mul_ops(&ops)
+}
+
+fn scan_for_mul(code: &str) -> Vec<(i32, i32)> {
+    let mul_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut ops: Vec<(i32, i32)> = vec![];
+    for (_, [op1, op2]) in mul_regex.captures_iter(code).map(|caps| caps.extract()){
+        ops.push((op1.parse::<i32>().unwrap(), op2.parse::<i32>().unwrap()));
+    }
+
+    ops
+}
+
+fn sum_mul_ops(ops: &Vec<(i32, i32)>) -> i32 {
+    ops.iter().map(|(op1, op2)| op1*op2).reduce(|sum, val| sum + val).unwrap()
+}
--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod common;
 mod day02;
+mod day03;
 
 fn main() {
     let (list1, list2) = day01::read_lists();
@@ -14,4 +15,13 @@ fn main() {
     let (safe_reports, safe_reports_extended) = day02::check_reports();
     println!("safe reports: {}", safe_reports);
     println!("safe reports extended {} ", safe_reports_extended);
+
+    // day 03
+    println!("ops");
+    let result = day03::calculate_mul();
+    println!("{}", result);
+
+    println!("restricted ops");
+    let result_restricted = day03::calculate_mul_restricted();
+    println!("{}", result_restricted);
 }
