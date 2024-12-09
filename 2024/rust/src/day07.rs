4d2cd64c3af04386b9b8a9a058626e8e1db355d0
Elias Boukamza <ezain2t@gmail.com>
2024-12-09 10:38:23 +0100

    day07

---
+++ 2024/rust/src/day07.rs
@@ -0,0 +1,76 @@
+use crate::common;
+
+pub fn read_lists() -> u64 {
+    let mut result_sum = 0;
+
+    if let Ok(lines) = common::read_lines("./input7.txt") {
+        for line in lines.flatten() {
+            let values = line.split(": ").collect::<Vec<&str>>();
+            let result = values[0].parse::<u64>().unwrap();
+            let left_values: Vec<_> = values[1].split(" ").map(|x| x.parse::<u64>().unwrap()).collect::<Vec<_>>();
+            let mut left_values_rev: Vec<_> = left_values.iter().rev().collect();
+            let first_elem =  left_values_rev.pop().unwrap();
+            let has_result = find_result(result, *first_elem, &left_values_rev);
+            if has_result {
+                result_sum += result
+            }
+        }
+    }
+    result_sum
+}
+
+pub fn read_lists2() -> u64 {
+    let mut result_sum = 0;
+
+    if let Ok(lines) = common::read_lines("./input7.txt") {
+        for line in lines.flatten() {
+            let values = line.split(": ").collect::<Vec<&str>>();
+            let result = values[0].parse::<u64>().unwrap();
+            let left_values: Vec<_> = values[1].split(" ").map(|x| x.parse::<u64>().unwrap()).collect::<Vec<_>>();
+            let mut left_values_rev: Vec<_> = left_values.iter().rev().collect();
+            let first_elem =  left_values_rev.pop().unwrap();
+            let has_result = find_result_concat(result, *first_elem, &left_values_rev);
+            if has_result {
+                result_sum += result
+            }
+        }
+    }
+    result_sum
+}
+
+fn find_result_concat(result: u64, partial: u64, list: &Vec<&u64>) -> bool {
+    if partial > result {
+        return false
+    }
+
+    let mut new_list = list.clone();
+    let last_elem = match new_list.pop() {
+        Some(el) => el,
+        None => return partial == result
+    };
+    let new_partial_1 = partial + last_elem;
+    let new_partial_2 = partial * last_elem;
+
+    let mut concat = partial.to_string();
+    concat.push_str(&last_elem.to_string());
+    let new_partial_3 = concat.parse::<u64>().unwrap();
+
+
+    find_result_concat(result, new_partial_1, &new_list) || find_result_concat(result, new_partial_2, &new_list) || find_result_concat(result, new_partial_3, &new_list)
+}
+
+fn find_result(result: u64, partial: u64, list: &Vec<&u64>) -> bool {
+    if partial > result {
+        return false
+    }
+    let mut new_list = list.clone();
+    let last_elem = match new_list.pop() {
+        Some(el) => el,
+        None => return partial == result
+    };
+    let new_partial_1 = partial + last_elem;
+    let new_partial_2 = partial * last_elem;
+
+    find_result(result, new_partial_1, &new_list) || find_result(result, new_partial_2, &new_list)
+}
+
--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -5,50 +5,55 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
 
 fn main() {
-    let (list1, list2) = day01::read_lists();
+    // let (list1, list2) = day01::read_lists();
     //day 01
-    println!("distance: ");
-    println!("{}", day01::calculate_distance(&list1, &list2));
-    println!("similarity: ");
-    println!("{}", day01::calculate_similarity(&list1, &list2));
+    // println!("distance: ");
+    // println!("{}", day01::calculate_distance(&list1, &list2));
+    // println!("similarity: ");
+    // println!("{}", day01::calculate_similarity(&list1, &list2));
 
     // day 02
-    let (safe_reports, safe_reports_extended) = day02::check_reports();
-    println!("safe reports: {}", safe_reports);
-    println!("safe reports extended {} ", safe_reports_extended);
+    // let (safe_reports, safe_reports_extended) = day02::check_reports();
+    // println!("safe reports: {}", safe_reports);
+    // println!("safe reports extended {} ", safe_reports_extended);
 
     // day 03
-    println!("ops");
-    let result = day03::calculate_mul();
-    println!("{}", result);
+    // println!("ops");
+    // let result = day03::calculate_mul();
+    // println!("{}", result);
 
-    println!("restricted ops");
-    let result_restricted = day03::calculate_mul_restricted();
-    println!("{}", result_restricted);
+    // println!("restricted ops");
+    // let result_restricted = day03::calculate_mul_restricted();
+    // println!("{}", result_restricted);
 
     // day 04
-    let sum = day04::find_xmas_all();
-    println!("XMAS count: {}", sum);
-
-    let sum_x = day04::find_mas_x();
-    println!("X-MAS count: {}", sum_x);
-
+    // let sum = day04::find_xmas_all();
+    // println!("XMAS count: {}", sum);
+    //
+    // let sum_x = day04::find_mas_x();
+    // println!("X-MAS count: {}", sum_x);
+    //
     // day 05
-    let sum_valid_orders = day05::sum_valid_orders();
-    println!("valid orders sum {}", sum_valid_orders);
-
-    let sum_invalid_orders_corrected = day05::sum_invalid_orders_corrected();
-    println!(
-        "invalid orders corrected sum {}",
-        sum_invalid_orders_corrected
-    );
+    // let sum_valid_orders = day05::sum_valid_orders();
+    // println!("valid orders sum {}", sum_valid_orders);
+    //
+    // let sum_invalid_orders_corrected = day05::sum_invalid_orders_corrected();
+    // println!(
+    //     "invalid orders corrected sum {}",
+    //     sum_invalid_orders_corrected
+    // );
 
     //day 6
-    let visited_positions = day06::get_positions_len();
-    println!("visited positions {}", visited_positions);
+    // let visited_positions = day06::get_positions_len();
+    // println!("visited positions {}", visited_positions);
+    //
+    // let num_of_obstacles = day06::get_num_of_obstacles();
+    // println!("num_of_obstacles {}", num_of_obstacles)
 
-    let num_of_obstacles = day06::get_num_of_obstacles();
-    println!("num_of_obstacles {}", num_of_obstacles)
+    //day 7
+    println!("{}", day07::read_lists());
+    println!("{}", day07::read_lists2());
 }
